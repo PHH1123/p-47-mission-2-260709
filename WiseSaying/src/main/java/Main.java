a4901f53a60c7bd470b882de3855ed76ab6cdba1
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-07-16 13:43:11 +0900

    9단계

--- WiseSaying/src/main/java/Main.java
+++ WiseSaying/src/main/java/Main.java
@@ -1,8 +1,14 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static final String path = System.getProperty("user.dir") + "/WiseSaying/src/main/java/db/wiseSaying/";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -45,7 +51,64 @@ public class Main {
         WiseSaying wiseSaying = new WiseSaying(wiseSayings.size() + 1, author, content);
         wiseSayings.add(wiseSaying);
 
+        jsonOutput(wiseSaying);
+        txtOutput(wiseSaying.getNo());
+
+
         System.out.println(wiseSaying.getNo() + "번 명언이 등록되었습니다.");
+    }
+
+    private static void jsonOutput(WiseSaying wiseSaying) {
+        File jsonFile = new File(path + wiseSaying.getNo() + ".json");
+
+        String json = "{\n" +
+                "\t\"id\": " + wiseSaying.getNo() + ",\n" +
+                "\t\"content\": \"" + wiseSaying.getContent() + "\",\n" +
+                "\t\"author\": \"" + wiseSaying.getAuthor() + "\"\n" +
+                "}";
+
+        fileOutput(jsonFile, json);
+    }
+
+    private static void txtOutput(int no) {
+        File txtFile = new File(path + "lastId.txt");
+
+        String txt = String.valueOf(no);
+
+        fileOutput(txtFile, txt);
+    }
+
+    private static void fileOutput(File file, String content) {
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+
+        try {
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+
+            byte[] bytes = content.getBytes();
+
+            bos.write(bytes, 0, bytes.length);
+
+        } catch (IOException e) {
+            System.out.println("***파일 저장에 실패했습니다.***");
+            System.out.println(e.getMessage());
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     private static void getList(List<WiseSaying> wiseSayings) {
@@ -95,6 +158,8 @@ public class Main {
             System.out.print("작가 : ");
             String author = sc.nextLine();
             wiseSaying.setAuthor(author);
+
+            jsonOutput(wiseSaying);
 
         } catch (RuntimeException e) {
             System.out.println(getId(input) + "번 명언은 존재하지 않습니다.");
